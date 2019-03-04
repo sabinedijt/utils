491f8d0884a64efb68b114a7f9de2d0fb702b9bc
sabinedijt <16683528+sabinedijt@users.noreply.github.com>
2019-03-04 11:33:54 +0100

    Update global.js

--- global.js
+++ global.js
@@ -1,4 +1,5 @@
 var globalQBCode = window.globalQBCode = {
+  //nonBounce start
   nonBounce: function(expOptions){
     var cm = window.globalQBCode.cm = window.globalQBCode.cm || require('cookieman'),
         path = window.location.pathname === '/' ? 'hp' : window.location.pathname,
@@ -22,39 +23,8 @@ var globalQBCode = window.globalQBCode = {
       })
     }
   },
-  jsPoller: function(items, callback, counter) {
-    var num = counter || 0
-    num++
-    var interval = ((num < 50) ? 100 : ((num < 70) ? 250 : (num < 80 ? 500 : undefined)))
-    var cbitems = []
-    items.forEach(function(e, i) {
-      if (typeof e === 'function' && e())
-        cbitems.push(e())
-      else if (typeof e === 'boolean' || typeof e === 'number' || typeof e === 'object')
-        cbitems.push(e)
-      else if (typeof e === 'string') {
-        if (!/^window\./i.test(e) && document.querySelectorAll(e).length > 0)
-          cbitems.push(document.querySelectorAll(e).length === 1 ? document.querySelector(e) : document.querySelectorAll(e))
-        else {
-          e = e.replace(/^window\./i, '')
-          var tmpval,
-              earr = /\./.test(e) ? e.split('.') : [e]
-          earr.forEach(function(element, index) {
-            var isFunction = /\(\)$/i.test(element)
-            element = isFunction ? element.slice(0, -2) : element
-            tmpval = (index === 0) ? window[element] : (tmpval !== undefined) ? tmpval[element] : undefined;
-            if(isFunction && tmpval !== undefined) tmpval = tmpval()
-          })
-          //if value is "" or 0 you still want it returned
-          tmpval !== undefined && cbitems.push(tmpval)
-        }
-      }
-    })
-    if (cbitems.length === items.length) callback.apply(this, cbitems)
-    else if (interval) setTimeout(function() {
-      this.jsPoller(items, callback, num)
-    }.bind(this), interval)
-  },
+  //nonBounce end
+  //jqPoller start
   jqPoller: function(items, callback, counter) {
     var num = counter || 0
     num++
@@ -91,5 +61,41 @@ var globalQBCode = window.globalQBCode = {
     } else if (interval) setTimeout(function() {
       this.jqPoller(items, callback, num)
     }.bind(this), interval)
+  },
+  //jqPoller end
+  //jsPoller start
+  jsPoller: function(items, callback, counter) {
+    var num = counter || 0
+    num++
+    var interval = ((num < 50) ? 100 : ((num < 70) ? 250 : (num < 80 ? 500 : undefined)))
+    var cbitems = []
+    items.forEach(function(e, i) {
+      if (typeof e === 'function' && e())
+        cbitems.push(e())
+      else if (typeof e === 'boolean' || typeof e === 'number' || typeof e === 'object')
+        cbitems.push(e)
+      else if (typeof e === 'string') {
+        if (!/^window\./i.test(e) && document.querySelectorAll(e).length > 0)
+          cbitems.push(document.querySelectorAll(e).length === 1 ? document.querySelector(e) : document.querySelectorAll(e))
+        else {
+          e = e.replace(/^window\./i, '')
+          var tmpval,
+              earr = /\./.test(e) ? e.split('.') : [e]
+          earr.forEach(function(element, index) {
+            var isFunction = /\(\)$/i.test(element)
+            element = isFunction ? element.slice(0, -2) : element
+            tmpval = (index === 0) ? window[element] : (tmpval !== undefined) ? tmpval[element] : undefined;
+            if(isFunction && tmpval !== undefined) tmpval = tmpval()
+          })
+          //if value is "" or 0 you still want it returned
+          tmpval !== undefined && cbitems.push(tmpval)
+        }
+      }
+    })
+    if (cbitems.length === items.length) callback.apply(this, cbitems)
+    else if (interval) setTimeout(function() {
+      this.jsPoller(items, callback, num)
+    }.bind(this), interval)
   }
+  //jsPoller end
 }
